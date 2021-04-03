0d4a1242df4f2437c4cb541eebc3a52d3707d3f5
lion <lion.twinkle.ge@gmail.com>
2021-04-03 02:32:20 +0200

    update email

--- js/contact.js
+++ js/contact.js
@@ -10,38 +10,28 @@ $(function () {
 
 
     // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
-
-        // if the validator does not prevent form submit
+    $('#contact-form #submit').on('click', function (e) {
         if (!e.isDefaultPrevented()) {
-            var url = "form/contact.php";
+             var sender = $('#contact-form #InputEmail').val();
+             var subject = $('#contact-form #InputSubject').val();
+             var message = $('#contact-form #InputMessage').val();
 
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
-
-                    // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
+             console.log(sender);
+             console.log(subject);
+             console.log(message);
+             Email.send({
+                Host: "smtp.elasticemail.com",
+                Username: "andrysoul32@gmail.com",
+                Password: "5DD0BDDE06710E36638FC46CBF7EFA75A3BC",
+                To: 'andrysoul32@gmail.com',
+                From: sender,
+                Subject: subject,
+                Body: message,
+              })
+            .then(function (message) {
+              alert("mail sent successfully")
             });
-            return false;
         }
+
     })
 });
